707c72a6326951c20b2991625c22f682b9c90e1b
Aurlin Dhillon <aurlindhillon@gmail.com>
2023-01-21 15:45:52 -0800

    reset project to blank state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-import Todo from "./components/Todo";
-import Modal from "./components/Modal";
-import Backdrop from "./components/BackDrop";
-
 function App() {
-  return (
-    <div>
-      <div>
-        <h1>My Todos</h1>
-        <Todo text="Learn React" />
-        <Todo text="Master React" />
-        <Todo text="Explore the React Course" />
-        <Modal />
-        <Backdrop />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
