9834346fb8ac61754bea80c26e5e37505ac39d88
Aurlin Dhillon <aurlindhillon@gmail.com>
2023-01-21 19:25:11 -0800

    refactored app to have layout component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import AllMeetups from "./pages/AllMeetups";
 import NewMeetup from "./pages/NewMeetup";
 import Favourites from "./pages/Favourites";
 import { useNavigate } from "react";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetups />} />
         <Route path="/new-meetup" element={<NewMeetup />} />
         <Route path="/favourites" element={<Favourites />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
