d53d5aaf204cde45f702ab2967239e1ba7ef2aff
Aurlin Dhillon <aurlindhillon@gmail.com>
2023-01-21 19:15:46 -0800

    Meetup item for each object in dummy data

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -3,16 +3,16 @@ import classes from "./MeetupItem.module.css";
 function MeetupItem(props) {
   return (
     <li className={classes.item}>
-      <div>
-        <img classes={classes.image} src={props.image} alt={props.title} />
-        <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
-          <p>{props.description}</p>
-        </div>
-        <div className={classes.actions}>
-          <button>To Favorites</button>
-        </div>
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>To Favorites</button>
       </div>
     </li>
   );
