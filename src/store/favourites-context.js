277c4d74e4e5772663ed50b5d3b266b0dae3ddf1
Aurlin Dhillon <aurlindhillon@gmail.com>
2023-01-23 20:11:45 -0800

    context methods are initialized and passed through context

--- src/store/favourites-context.js
+++ src/store/favourites-context.js
@@ -3,6 +3,9 @@ import { createContext, useState } from "react";
 const FavouritesContext = createContext({
   favourites: [],
   totalFavourites: 0,
+  addFavourite: (favouriteMeetup) => {},
+  removeFavourite: (meetupId) => {},
+  itemIsFavourite: (meetupId) => {},
 });
 
 function FavouritesContextProvider(props) {
@@ -31,6 +34,9 @@ function FavouritesContextProvider(props) {
   const context = {
     favourites: userFavourites,
     totalFavourites: userFavourites.length,
+    addFavourite: addFavouriteHandler,
+    removeFavourite: removeFavouriteHandler,
+    itemIsFavourite: itemIsFavouriteHandler,
   };
 
   return (
